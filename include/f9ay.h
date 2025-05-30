e1b1d1310942a5fdbd96febe0c84a2fe3b657646
TonyLiu0430 <104262003+TonyLiu0430@users.noreply.github.com>
2025-05-30 17:09:57 +0800

    [feat] add png support (#4)

--- include/f9ay.h
+++ include/f9ay.h
@@ -10,7 +10,7 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-    F9AY_API char* f9ay_read(const char *filename, int *row, int *col);
+    F9AY_API char* f9ay_read(const char *filename, int *row, int *col, int *channles);
     F9AY_API int f9ay_bmp_export(const char *filename, const char *data, int row, int col, int channels);
     F9AY_API int f9ay_jpeg_export(const char *filename, const char *data, int row, int col, int channels);
     F9AY_API int f9ay_png_export(const char *filename, const char *data, int row, int col, int channels);
